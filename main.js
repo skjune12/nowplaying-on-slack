c93ad3611e0c9f2cebd965dda0df73c6e1ef7248
Kohei Suzuki <jingle@sfc.wide.ad.jp>
2018-02-22 00:56:50 +0900

    enable to send multiple slack team

--- main.js
+++ main.js
@@ -1,65 +1,75 @@
 const request = require('request');
+const yaml = require('js-yaml');
+const fs = require('fs');
 const { exec } = require('child_process');
 
-const token = process.env.SLACK_API_TOKEN;
+// const token = process.env.SLACK_API_TOKEN;
+const tokens = yaml.safeLoad(fs.readFileSync('./tokens.yaml', 'utf8'));
 const spotifyScript = './bin/get-playing-on-spotify';
 
-const sendToSlack = ({ emoji, message }) => {
-  request({
-    url: 'https://slack.com/api/users.profile.set',
-    method: 'POST',
-    form: {
-      token,
-      profile: JSON.stringify({
-        status_text: message,
-        status_emoji: emoji,
-      }),
-    },
-  }, (error, response, body) => {
-    if (!error) {
-      return;
-    }
+const sendToSlack = ({ token, emoji, message }) => {
+    request({
+        url: 'https://slack.com/api/users.profile.set',
+        method: 'POST',
+        form: {
+            token,
+            profile: JSON.stringify({
+                status_text: message,
+                status_emoji: emoji,
+            }),
+        },
+    }, (error, response, body) => {
+        if (!error) {
+            return;
+        }
 
-    console.error(error, response, body);
-  });
+        console.error(error, response, body);
+    });
 };
 
 const watchSpotify = () => {
-  let before;
+    let before;
 
-  setInterval(() => {
-    exec(spotifyScript, (e, stdout, stderr) => {
-      if (e) {
-        console.error(e);
-        return;
-      }
-      if (stderr) {
-        console.error(stderr);
-        return;
-      }
+    setInterval(() => {
+        exec(spotifyScript, (e, stdout, stderr) => {
+            if (e) {
+                console.error(e);
+                return;
+            }
+            if (stderr) {
+                console.error(stderr);
+                return;
+            }
 
-      const m = JSON.parse(stdout);
-      if ('error' in m) {
-        console.error(m.error);
-        return;
-      }
+            const m = JSON.parse(stdout);
+            if ('error' in m) {
+                console.error(m.error);
+                return;
+            }
 
-      if (before === JSON.stringify(m)) {
-        // console.log('not change');
-        return;
-      }
-      before = JSON.stringify(m);
-      const message = `${m.track} - ${m.artist}`;
-      const emoji = (m.state === 'playing') ? ':spotify:' : ':musical_note:';
-      sendToSlack({ message, emoji });
-      console.log(m);
-    });
-  }, 3000);
+            if (before === JSON.stringify(m)) {
+                // console.log('not change');
+                return;
+            }
+            before = JSON.stringify(m);
+            const message = `${m.track} - ${m.artist}`;
+            const emoji = (m.state === 'playing') ? ':headphones:' : ':musical_note:';
+            try {
+                for (i in tokens) {
+                    var token = tokens[i];
+                    sendToSlack({ token, message, emoji });
+                }
+            } catch (e) {
+                console.log(e);
+            }
+            console.log(m);
+        });
+    }, 3000);
 };
 
-if (!token) {
-  console.log('token ga naiyo');
-  process.exit();
+if (!tokens) {
+    console.log('token ga naiyo');
+    process.exit();
 }
 
 watchSpotify();
